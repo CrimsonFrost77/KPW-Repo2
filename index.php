817d8e7da53a0c5edd85adb61cbd6fa7f61513ee
CrimsonFrost77 <grayishbluegamer@gmail.com>
2024-10-11 02:08:25 +0600

    seoerated general css

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
   <head>
     <meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="navbar.css">
     <link rel="stylesheet" href="general.css">
+    <link rel="stylesheet" href="navbar.css">
 		<title>Kaliakair Paurashava</title>
   </head>
   <body>
     <?php include 'navbar.html'; ?>
-    <p>kotha</p><p>kotha</p><p>kotha</p><p>kotha</p><p>kotha</p>
-    <p>kotha</p><p>kotha</p><p>kotha</p><p>kotha</p><p>kotha</p>
-    <p>sroooooooooosdfsdfsgh s sd fs fs dfsfskotha</p>
+    <div class="body-section">
+      <p>kotha</p><p>kotha</p><p>kotha</p><p>kotha</p><p>kotha</p>
+      <p>kotha</p><p>kotha</p><p>kotha</p><p>kotha</p><p>kotha</p>
+      <p>sroooooooooosdfsdfsgh s sd fs fs dfsfskotha</p>
+    </div>
+    
   </body>
 </html>
